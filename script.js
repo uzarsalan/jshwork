ebd86495740c86d8deb2474103cb9709f91cdae3
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-02-10 14:40:43 +0700

    jsscripthw8

---
+++ script.js
@@ -0,0 +1,14 @@
+const NAME = "Evgeniy"
+const DOB = 2023 - 1994
+const OCCUPITION = "Jewellry master"
+console. log(NAME)
+console. log(DOB)
+console. log(OCCUPITION)
+let strExample = `${NAME} ${DOB} old ${OCCUPITION}`
+console.log(strExample)
+
+
+
+
+
+
