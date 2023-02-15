9b15f890d3e4f4e84db00ccefb3f3c2680910181
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-02-15 18:55:46 +0700

    jsobjmass

---
+++ menurest/script.js
@@ -0,0 +1,35 @@
+let kebab = {
+    name: "kebab",
+    recept: "chiken, pita, chees, vegetables",
+    inprice: 50,
+    price: 150
+}
+let recept1 = [chiken, pite, chees, vegetables]
+let plov = {
+    name: "plov",
+    recept: "meat, rice, vegetables",
+    inprice: 80,
+    price: 250
+}
+let recept2 = [meat, rice, vegetables]
+let noodleSoup = {
+    name: "noodleSoup",
+    recept: "noodle, shrimp, vegetables",
+    inprice: 50,    
+    price: 200
+}
+let receipt3 = [noodle, shrimp,vegetables]
+let buuzas = {
+    name: "buuzas",
+    recept: "meat, dough",
+    inprice: 15,
+    price: 50
+}
+let recept4 = [meat, dought]
+let menuFood = [kebab, plov, noodleSoup, buuzas]
+console.log(menuFood)
+
+
+
+
+
