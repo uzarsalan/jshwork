69ecc7941c6aaa7f7aad17c3e3bbf57f8c839629
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-02-16 18:23:23 +0700

    jsscrobjmass2

--- menurest/script.js
+++ menurest/script.js
@@ -1,35 +1,49 @@
-let kebab = {
+const kebab = {
     name: "kebab",
-    recept: "chiken, pita, chees, vegetables",
+    receipt: "chiken, vegetables, sauce",
     inprice: 50,
-    price: 150
-}
-let recept1 = [chiken, pite, chees, vegetables]
-let plov = {
-    name: "plov",
-    recept: "meat, rice, vegetables",
-    inprice: 80,
-    price: 250
-}
-let recept2 = [meat, rice, vegetables]
-let noodleSoup = {
-    name: "noodleSoup",
-    recept: "noodle, shrimp, vegetables",
-    inprice: 50,    
-    price: 200
-}
-let receipt3 = [noodle, shrimp,vegetables]
-let buuzas = {
-    name: "buuzas",
-    recept: "meat, dough",
-    inprice: 15,
-    price: 50
-}
-let recept4 = [meat, dought]
-let menuFood = [kebab, plov, noodleSoup, buuzas]
-console.log(menuFood)
+    price: 150,
+};
+console.log(kebab);
+let keys = Object.keys(kebab);
+console.log(keys);
 
+let values = Object.values(kebab);
+console.log(values);
 
+let entries = Object.entries(kebab);
+console.log(entries);
 
+const plov ={
+    name1: "plov",
+    receipt1: "meat, rice, vegetables",
+    inprice1: 80,
+    price1: 240,
+};
+console.log(plov);
+let keys1 = Object.keys(plov);
+console.log(keys);
+
+let values1 = Object.values(plov);
+console.log(values);
+
+let entries1 = Object.entries(plov);
+console.log(entries);
+
+const buuzas = {
+    name2: "buuzas",
+    receipt2: "meat, dough",
+    inprice2: 25,
+    price2:60,
+};
+console.log(buuzas);
+let keys2 = Object.keys(buuzas);
+console.log(keys);
+
+let values2 = Object.values(buuzas);
+console.log(values);
+
+let entries2 = Object.entries(buuzas);
+console.log(entries);
 
 
